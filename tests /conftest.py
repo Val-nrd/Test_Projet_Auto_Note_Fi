21ecc280e1354819c5224d1dfb635f4a7bfe03f4
Val-nrd <valentin.normand@edhec.com>
2025-05-06 22:50:56 +0200

    Update conftest.py

--- tests /conftest.py
+++ tests /conftest.py
@@ -1,1 +1,14 @@
+import os, shutil, pytest
+from fastapi.testclient import TestClient
+from backend.main import app, UPLOAD_DIR
+
+client = TestClient(app)
+
+@pytest.fixture(autouse=True)
+def cleanup_upload_dir():
+    if os.path.exists(UPLOAD_DIR):
+        shutil.rmtree(UPLOAD_DIR)
+    os.makedirs(UPLOAD_DIR, exist_ok=True)
+    yield
+    shutil.rmtree(UPLOAD_DIR)
 
